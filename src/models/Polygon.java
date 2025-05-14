1c2df466fb02b43fc87ebe274a8e5bfcbdcbb7eb
Stepiiss <hemerkastepan@gmail.com>
2025-05-14 17:14:13 +0200

    Oprava casti kodu

--- src/models/Polygon.java
+++ src/models/Polygon.java
@@ -12,9 +12,6 @@ public class Polygon {
         vertices = new ArrayList<>();
     }
 
-    public Polygon(ArrayList<Point> vertices) {
-        this.vertices = vertices;
-    }
 
     public void addPoint(Point point) {
         vertices.add(point);
@@ -24,12 +21,5 @@ public class Polygon {
         return vertices;
     }
 
-    public void draw(LineCanvasRasterizer rasterizer, Color color) {
-        for (int i = 0; i < vertices.size(); i++) {
-            Point start = vertices.get(i);
-            Point end = vertices.get((i + 1) % vertices.size());
-            Line line = new Line(start, end, color);
-            rasterizer.rasterizeLine(line);
-        }
-    }
+
 }
--- src/rasterizers/LineCanvasRasterizer.java
+++ src/rasterizers/LineCanvasRasterizer.java
@@ -21,33 +21,11 @@ public class LineCanvasRasterizer {
         dottedLineRasterizer = new DottedLineRasterizerTrivial(raster);
     }
 
-    public void rasterizeCanvas(LineCanvas canvas) {
-        lineRasterizer.rasterizeArray(canvas.getLines());
-        dottedLineRasterizer.rasterizeArray(canvas.getDottedLines());
-        for(Polygon polygon : canvas.getPolygons()) {
-            this.rasterizePolygon(polygon);
-        }
-    }
 
     public void rasterizeLine(Line line) {
         lineRasterizer.rasterize(line);
     }
 
-    public void rasterizePolygon(Polygon polygon) {
-        models.Point prevPoint = null;
-        for (models.Point point : polygon.getPoints()) {
-            if(prevPoint == null) {
-                prevPoint = point;
-                continue;
-            }
-
-            lineRasterizer.rasterize(new Line(prevPoint, point, Color.white));
-
-            prevPoint = point;
-        }
-
-        lineRasterizer.rasterize(new Line(prevPoint, polygon.getPoints().getFirst(), Color.white));
-    }
 
     public void rasterizeDottedLine(Line line) {
         dottedLineRasterizer.rasterize(line);
